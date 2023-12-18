2e067a49d2714f51616bf47a13a1ae1bcb685d54
franklinoli <f.oliverosafonso2@gmail.com>
2023-12-18 20:25:17 +0100

    ...2

--- TrabajoGit/src/main/java/Prueba.java
+++ TrabajoGit/src/main/java/Prueba.java
@@ -1,5 +1,5 @@
 public class Prueba {
     public static void main(String[] args) {
-        System.out.println("HolaMundoejemplo22");
+        System.out.println("HolaMundoejemplo22555");
     }
 }
